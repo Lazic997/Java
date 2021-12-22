d9d2f23fad066a61ecf0195cd5817f19729e03e0
Lazic997 <83877809+Lazic997@users.noreply.github.com>
2021-12-22 15:51:56 +0100

    Update Raketa.java

--- Uvod u visenitnost/src/Zadatak3/Raketa.java
+++ Uvod u visenitnost/src/Zadatak3/Raketa.java
@@ -1,37 +1,26 @@
-package Zadatak3;
+package zadatak3;
 
-public class Raketa implements Runnable{
-
-    private Thread t;
+public class Raketa implements Runnable {
+    
     private String ime;
     private LansirnaRampa rampa;
 
-    public Raketa(String ime,LansirnaRampa rampa){
+    public Raketa(String ime, LansirnaRampa rampa) {
+        
         this.ime = ime;
         this.rampa = rampa;
+        
     }
-
+    
     @Override
     public void run(){
-            
+        
+        System.out.println("Raketa: " + ime + ", uskoro krece lansiranje!");
         try{
-            rampa.lansiranje(); // metodu mozemo sihnronizirati i tako da ovu liniju koda stavimo u synchronized blok
+            rampa.lansiraj(); // metodu mozemo sihnronizirati i tako da ovu liniju koda stavimo u synchronized blok
         }catch(Exception e){
-            System.out.println("Greska pri lansiranju!");
         }
-        
-        System.out.println(this.ime + " raketa je uspjesno lansirana!");
-        
+        System.out.println("Raketa: " + ime + ", uspjesno lansirana!");
     }
-
-    public void start(){
-
-        System.out.println("Pocinje lansiranje: " + ime);
-        if(t == null){
-            t = new Thread(this,ime);
-            t.start();   
-        }
-    }
-
-
+    
 }
