95a151cf63b01f541276ca1f12aa926a4cc794b5
Lazic997 <83877809+Lazic997@users.noreply.github.com>
2021-12-22 15:52:25 +0100

    Update App.java

--- Uvod u visenitnost/src/Zadatak3/App.java
+++ Uvod u visenitnost/src/Zadatak3/App.java
@@ -4,12 +4,10 @@ public class App {
     public static void main(String[] args){
 
         LansirnaRampa rampa = new LansirnaRampa();
-        Raketa thread1 = new Raketa("Raketa1" , rampa);
-        Raketa thread2 = new Raketa("Raketa2" , rampa);
-
-
-        thread1.start();
+        Thread thread1 = new Thread(new Raketa("prva", rampa));
+        Thread thread2 = new Thread(new Raketa("druga", rampa));
         
+        thread1.start();
         thread2.start();
     }
 }
