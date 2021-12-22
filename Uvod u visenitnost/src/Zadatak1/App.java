4e48add3047568e523627baadc23df612aab0043
Lazic997 <83877809+Lazic997@users.noreply.github.com>
2021-12-22 15:49:51 +0100

    Update App.java

--- Uvod u visenitnost/src/Zadatak1/App.java
+++ Uvod u visenitnost/src/Zadatak1/App.java
@@ -3,12 +3,10 @@ package Zadatak1;
 public class App {
     public static void main(String[] args){
 
-        Raketa raketa1 = new Raketa();
-        Raketa raketa2 = new Raketa();
+        LansirnaRampa rampa = new LansirnaRampa();
+        Raketa raketa1 = new Raketa("prva", rampa);
+        Raketa raketa2 = new Raketa("druga", rampa);
         
-        raketa1.setIme("Prva");
-        raketa2.setIme("Druga");
-
         raketa1.start();
         raketa2.start();
     }
